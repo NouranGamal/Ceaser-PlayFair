3fbcffe2b6d2482d096d3aa42e07fe8313ae5627
NouranGamal <56848533+NouranGamal@users.noreply.github.com>
2019-10-21 23:29:34 +0200

    Add files via upload

---
+++ caeserClass.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author win10
+ */
+public class caeserClass {
+     public static String encrypt(String text, int s) 
+    { 
+        String result= ""; 
+  
+        for (int i=0; i<text.length(); i++) 
+        { 
+            if (Character.isUpperCase(text.charAt(i))) 
+            { 
+                result+= (char)(((int)text.charAt(i) + 
+                                  s - 65) % 26 + 65); 
+               
+            } 
+            else
+            { 
+                result+= (char)(((int)text.charAt(i) + 
+                                  s - 97) % 26 + 97); 
+                
+            } 
+        } 
+        return result;
+    }
+     public static String decrypt(String text, int s) 
+    { 
+        String result= ""; 
+  
+        for (int i=0; i<text.length(); i++) 
+        { 
+            if (Character.isUpperCase(text.charAt(i))) 
+            { 
+                result+= (char)(((int)text.charAt(i) - 
+                                  s -65) % 26 +65); 
+               
+            } 
+            else
+            { 
+                result+= (char)(((int)text.charAt(i) - 
+                                  s - 97) % 26 +97); 
+                
+            } 
+        } 
+        return result;
+    }
+}
